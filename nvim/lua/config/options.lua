55fd733ab8501d09a700a05f0db0efd4687cf29c
andycancado <andycancado@gmail.com>
2024-09-08 17:30:26 +0100

    add scroll opt

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -35,6 +35,8 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 
+vim.opt.scrolloff = 8
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
